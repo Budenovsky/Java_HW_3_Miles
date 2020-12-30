ae600ccef9b5bce98a359610690f45a46ecda095
Boris.Budenovsky <fpqb00@gmail.com>
2020-12-30 11:14:46 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2499;
+        int milesBonus = ticketPrice / 20;
+        System.out.println(milesBonus);
+    }
+}
